086cb67e36eee76b3dee205127dd5983fc9ee879
Khoi T. Nguyen <thorwaitson@gmail.com>
2023-03-22 10:36:47 -0500

    modified:   public/js/script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,20 @@
 const responseField = document.querySelector("#responseField");
 const submitButton = document.querySelector("#submit");
 
-function sendPlayerInfo() {
-  const playerScore = document.getElementById("player-score").innerHTML;
+function sendPlayerInfo(event) {
+  event.preventDefault();
+
+  const playerScore = document.getElementById("player-score").innerText || 0;
   const playername = document.getElementById("playername").value;
 
-  let xhr = new XMLHttpRequest();
-  let url = "/leaderboard";
-  let data = JSON.stringify({ score: playerScore, playername: playername });
+  const xhr = new XMLHttpRequest();
+  const url = "/api/leaderboard";
+  const data = JSON.stringify({ score: playerScore, playername: playername });
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      console.log(xhr.response);
+      const response = JSON.parse(xhr.response);
+      if (response.success) window.location.reload();
     }
   };
 
